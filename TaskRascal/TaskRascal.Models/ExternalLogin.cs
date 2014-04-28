8e2c5529409b5c818d8bc8d8f8d00693896468ed
Mark Dewey <mdewey@problemsolutions.net>
2014-04-28 08:29:02 -0400

    moved some files around

--- TaskRascal/TaskRascal.Models/ExternalLogin.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskRascal.Models
-{
-    public class ExternalLogin
-    {
-        public string Provider { get; set; }
-        public string ProviderDisplayName { get; set; }
-        public string ProviderUserId { get; set; }
-    }
-}
