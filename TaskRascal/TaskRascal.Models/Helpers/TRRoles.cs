aa2ebe7eaf6e85297a0a7a1652759e0ce2993ad3
Mark Dewey <mdewey@problemsolutions.net>
2014-04-28 22:20:46 -0400

    added files for simple memebership

---
+++ TaskRascal/TaskRascal.Models/Helpers/TRRoles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskRascal.Models.Helpers
+{
+    public class TRRoles
+    {
+        public const String AdminSystemName = "admin";
+        public const String UserSystemName = "user";
+        public const String ParentRole = "parent";
+        public const String KidRole = "child";
+
+        public static List<String> GetAllRoles()
+        {
+            return new List<string>() { AdminSystemName, UserSystemName, ParentRole, KidRole};
+        }
+    }
+}
---
+++ TaskRascal/TaskRascal/Migrations/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskRascal.Migrations
+{
+    public class Data
+    {
+        public static IEnumerable<Configuration.user> UserList
+        {
+            get
+            {
+                return new List<Configuration.user>(){ 
+                    new Configuration.user("admin@tr.com", "password", TaskRascal.Models.Helpers.TRRoles.GetAllRoles(), "Super", "User"),
+                    new Configuration.user( "user@tr.com", "password", new List<string>(){TaskRascal.Models.Helpers.TRRoles.UserSystemName} , "User", "Smith")
+                };
+            }
+        }
+    }
+}
